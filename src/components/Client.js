1b335d17e353ef8e9609cc112d81315b3f88868a
Niranjan Kumar <ns04001@gmail.com>
2023-02-28 10:41:31 +0530

    socket file updated

--- src/components/Client.js
+++ src/components/Client.js
@@ -1,23 +1,9 @@
-// import React from "react";
-// import Avatar from "react-avatar";
-
-// const Client = ({ username }) => {
-//   return (
-//     <div className="client">
-//       <Avatar name={username} size={50} round="14px" />
-//       <span className="userName">{username}</span>
-//     </div>
-//   );
-// };
-
-// export default Client;
 import React from "react";
 import Avatar from "react-avatar";
 
 const Client = ({ username }) => {
   return (
     <div className="client">
-      {/* Avatar from react library */}
       <Avatar name={username} size={50} round="14px" />
       <span className="userName">{username}</span>
     </div>
